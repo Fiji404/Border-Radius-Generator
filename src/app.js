2ab6fe88d617dac3ee53930a3581b9d3a82f95cb
Radek <radek42310o@gmail.com>
2022-05-05 16:19:08 +0200

    fixes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 const changeSizeOfElement = document.querySelector('.rounded-item_change-size');
 const changeSizePanel = document.querySelector('.round-item-size-inputs');
 const elementPreview = document.querySelector('.rounded-item');
-const elementSizeValues = document.querySelectorAll('.input-value.size');
-const leftTopRadiusValue = document.querySelector('.input-value.top-left-radius')
-const rightTopRadiusValue = document.querySelector('.input-value.top-right-radius')
-const bottomLeftRadiusValue = document.querySelector('.input-value.bottom-left-radius')
-const bottomRightRadiusValue = document.querySelector('.input-value.bottom-right-radius')
+const inputSizeValues = document.querySelectorAll('.input-value');
 
 changeSizeOfElement.addEventListener('click', () => {
     changeSizePanel.classList.toggle('active');
-})
+});
 
-elementSizeValues.forEach(sizeInput => {
+inputSizeValues.forEach(sizeInput => {
     sizeInput.addEventListener('input', () => {
-        const dimensionName = sizeInput.getAttribute('data-dimension');
-        elementPreview.style[dimensionName] = `${sizeInput.value}px`
-    })
-})
+        const placeName = sizeInput.dataset.place;
+        elementPreview.style[placeName] = `${sizeInput.value}px`;
+    });
+});
+
+
