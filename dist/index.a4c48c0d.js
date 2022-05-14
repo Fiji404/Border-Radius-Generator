1f6124e01a3b2f3b9b49e7eaa32807191263eb84
Radek <radek42310o@gmail.com>
2022-05-14 21:27:01 +0200

    dark mode refuse

--- dist/index.a4c48c0d.js
+++ dist/index.a4c48c0d.js
@@ -529,12 +529,16 @@ function hmrAcceptRun(bundle, id) {
 const bodyElement = document.querySelector('body');
 const changeThemeBtn = document.querySelector('.theme-toggle');
 changeThemeBtn.addEventListener('click', ()=>{
+    let documentStyle = getComputedStyle(document.documentElement);
+    console.log(documentStyle.getPropertyValue('--white'));
     if (bodyElement.classList.contains('light-theme')) {
         bodyElement.classList.replace('light-theme', 'dark-theme');
+        bodyElement.dataset.darkmode = 'true';
         changeThemeBtn.classList.add('active');
     } else {
         bodyElement.classList.replace('dark-theme', 'light-theme');
         changeThemeBtn.classList.remove('active');
+        bodyElement.dataset.darkmode = 'false';
     }
 });
 
